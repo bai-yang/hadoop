9f6b7d3d102bf462e2214a8034e735f86d10973b
baiyang <baiyang.java@gmail.com>
2017-03-07 20:58:29 +0800

    add urlcat

---
+++ src/main/java/com/a/eye/by/hdfs/UrlCat.java
@@ -0,0 +1,29 @@
+package com.a.eye.by.hdfs;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+public class UrlCat {
+
+    static {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main(String[] args) {
+        InputStream in = null;
+
+        try {
+            in = new URL(args[0]).openStream();
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeStream(in);
+        }
+
+    }
+
+}
